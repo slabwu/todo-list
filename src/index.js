e9bc2718864aa4b3055f23bdd8478b594f29de36
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-14 00:50:03 +0800

    Connect dialog to addToDoTo() method

--- src/index.js
+++ src/index.js
@@ -4,19 +4,31 @@ import { go } from "./todos";
 import { screen } from "./interface";
 
 export const Display = (function() {
+    const toDoDialog = document.getElementById("toDoDialog");
 
     // screen.createButton("Add Project", content, () => {
     //     go.addProject("Test");
     //     console.log(go.getProjects());
     // });
-    screen.createButton("Add To Do", content, () => {
-        go.addToDoTo("Default", "Dishes", "Clean the dishes")
-        console.log(go.getToDosFrom("Default"));
-        screen.loadToDos("Default");
-    });
 
+    screen.createButton("Add To Do", content, () => {
+        //go.addToDoTo("Default", "Dishes", "Clean the dishes")
+        //screen.loadToDos("Default");
+        toDoDialog.showModal();
+    });
 
     go.addToDoTo("Default", "Eat", "Finish dinner");
     go.addToDoTo("Default", "Walk", "Walk the dog");
     screen.loadToDos("Default");
+})();
+
+const Dialog = (function() {
+    confirmBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        const titleInput = document.getElementById("title").value;
+        const descriptionInput = document.getElementById("description").value;
+        go.addToDoTo("Default", titleInput, descriptionInput);
+        screen.loadToDos("Default");
+        toDoDialog.close();
+    });    
 })();
