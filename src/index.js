6180383a5e7a3ae5e6b7f3309e1df3666b1b477d
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-13 21:54:18 +0800

    Add addToDo method

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ class Controller {
 
     constructor() {
         this.#projects.push(new Project("Default"));
+        this.#projects.push(new Project("2"));
+        this.#projects.push(new Project("3"));
+    }
+
+    addToDo(title, description, project) {
+        let currentProject = this.#projects.find((element) => element.title === project);
+        if (!currentProject) throw new Error('Project does not exist!');
+        currentProject.addToDo(title, description);
+        console.log(currentProject);
     }
 
     getProjects = () => this.#projects;
@@ -16,19 +25,26 @@ class Controller {
 // }
 
 class Project {
+    #todos = [];
+
     constructor(title) {
         this.title = title;
     }
+
+    getToDos = () => this.#todos;
+
+    addToDo(title, description) {
+        this.#todos.push(new ToDo(title, description));
+    }
 }
 
-// class ToDo {
-//     constructor(title, description) {
-        
-//     }
-// }
+class ToDo {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+    }
+}
 
 const go = new Controller();
 
-
-console.log(go.getProjects())
 
