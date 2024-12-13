04b8d8fe11f344d0ca8361bb6858c2aac75efb6a
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-13 22:41:30 +0800

    Create Add To Do button in DOM

--- src/index.js
+++ src/index.js
@@ -24,7 +24,6 @@ class Controller {
 }
 
 class Interface {
-    //content = document.getElementById("content");
     constructor() {
         const content = document.createElement("div")
         document.body.appendChild(content)
@@ -36,6 +35,14 @@ class Interface {
         addProject.addEventListener("click", () => {
             go.addProject("Test");
             console.log(go.getProjects());
+        })
+
+        const addToDo = document.createElement("button");
+        addToDo.textContent = "Add To Do";
+        content.appendChild(addToDo);
+        addToDo.addEventListener("click", () => {
+            go.addToDoTo("Default", "Dishes", "Clean the dishes")
+            console.log(go.getToDosFrom("Default"));
         })
     }
 }
