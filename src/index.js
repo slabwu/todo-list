d43e2638076113968e4392aba2fada998fe6598b
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-13 21:20:15 +0800

    Add Controller & Project classes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 import "./styles.css";
 import { compareAsc, format } from "date-fns";
+
+class Controller {
+    projects = [];
+    defaultProject = new Project("Default");
+
+    constructor() {
+        this.projects.push(this.defaultProject);
+    }
+    test() {
+        console.log('works');
+    }
+
+    getProjects = () => this.projects;
+}
+
+// class Interface {
+    
+// }
+
+class Project {
+    constructor(title) {
+        this.title = title;
+    }
+}
+
+// class ToDo {
+//     constructor(title, description) {
+        
+//     }
+// }
+
+const go = new Controller();
+
+console.log(go.defaultProject)
+console.log(go.projects)
+
