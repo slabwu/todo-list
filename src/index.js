3d03fa262b64dcecaf8d983b23d8bd3a47c4d852
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-14 11:59:57 +0800

    Implement pubsub module

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import "./styles.css";
 import { compareAsc, format } from "date-fns";
 import { Task, Tasks, addTask } from "./task";
 import { test } from "./helper";
+import { Events } from "./pubsub";
 
 addTask("test", "test", "Default");
 addTask("test", "test", "Default");
 addTask("test", "test", "Default");
-test(Tasks.list);
+//test(Tasks.list);
+
+Events.on("meow", test)
+Events.on("meow", addTask)
+Events.off("meow", addTask)
+Events.emit("meow", "works!")
