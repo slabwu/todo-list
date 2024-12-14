4a1c7388e754d61aec8db3e9a5a1c8826e7d176c
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-14 11:03:28 +0800

    Create helper function module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import { compareAsc, format } from "date-fns";
-import { Task, addTask } from "./task";
+import { Task, addTask, List } from "./task";
+import { test } from "./helper";
 
 addTask("test", "test", "Default");
+test(List.tasks);
